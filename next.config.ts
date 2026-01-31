d0ddc10d81630ad7cf5f994d72424f4ca21e1a85
MarkC147 <mark147chantel@gmail.com>
2026-01-31 17:32:40 +0000

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Required for static export if using the Next.js Image component
+  },
+  basePath: "/website-car-trader", // Set the base path for GitHub Pages
+  assetPrefix: "/website-car-trader/", // Set the asset prefix for GitHub Pages
 };
 
 export default nextConfig;
